840811f9f2b716daf83b048ed82d873622d810f3
SuperbatP <wjssnfl0504@gmail.com>
2023-06-15 10:09:27 +0900

    클래스의 생성자 참조-기본 생성자 참조

---
+++ lambdaexpression/src/sec01_lambdaexpression/EX09/RefOfClassConstructor_1.java
@@ -0,0 +1,24 @@
+package sec01_lambdaexpression.EX09;
+
+interface A{B abc(int k);}
+class B{
+	B(){System.out.println("첫 번째 생성자");}
+	B(int k){System.out.println("두 번째 생성자");}
+}
+
+public class RefOfClassConstructor_1 {
+public static void main(String[] args) {
+	//익명이너클래스
+	A a1 = new A() {@Override
+	public B abc(int k) {
+		return new B(3);
+	}};
+	//람다식
+	A a2 =(int k)->new B(3);
+	//클래스 생성자 참조
+	A a3=B::new;
+	a1.abc(3);
+	a2.abc(3);
+	a3.abc(3);
+}
+}
