9f4dba03e0ff207e7bb7de1a662970be042c7938
SuperbatP <wjssnfl0504@gmail.com>
2023-06-15 10:08:56 +0900

    배열 생성자 참조

---
+++ lambdaexpression/src/sec01_lambdaexpression/EX08/RefOfArrayConstructor.java
@@ -0,0 +1,21 @@
+package sec01_lambdaexpression.EX08;
+
+interface A{int[] abc(int len);}
+
+public class RefOfArrayConstructor {
+public static void main(String[] args) {
+	//익명이너클래스
+	A a1=new A() {@Override
+	public int[] abc(int len) {return new int[len];}};
+	//람다식
+	A a2 =(int len)->{return new int[len];};
+	//배열의 생성자 참조
+	A a3 =int[]::new;
+	int[] array1 = a1.abc(3);
+	System.out.println(array1.length);
+	int[] array2 = a2.abc(3);
+	System.out.println(array2.length);
+	int[] array3 = a3.abc(3);
+	System.out.println(array3.length);
+}
+}
